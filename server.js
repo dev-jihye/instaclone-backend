b65fd14e036911f8161063565da3db286d045810
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-02 18:37:28 +0900

    ⚡️ 서버에서 들어오는 모든 요청 보기

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 import express from 'express';
+import logger from 'morgan';
 import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './schema';
 import { getUser, protectResolver } from './users/users.utils';
@@ -8,6 +9,7 @@ import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 const PORT = process.env.PORT;
 const startServer = async () => {
   const app = express();
+  app.use(logger('dev'));
   app.use(graphqlUploadExpress());
 
   const apollo = new ApolloServer({
