76194b44e7ef476a670c2857531546bb7d8e1187
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-02 15:10:17 +0900

    ✨ file upload

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 require('dotenv').config();
-import { ApolloServer } from 'apollo-server';
-import schema from './schema';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './schema';
 import { getUser, protectResolver } from './users/users.utils';
-
-const server = new ApolloServer({
-  schema,
-  context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-      protectResolver,
-    };
-  },
-});
+import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 
 const PORT = process.env.PORT;
+const startServer = async () => {
+  const app = express();
+  app.use(graphqlUploadExpress());
 
-server.listen(PORT).then(() => console.log(`🚀 Server ready at ${PORT} ✅`));
+  const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+    context: async ({ req }) => {
+      return {
+        loggedInUser: await getUser(req.headers.token),
+        protectResolver,
+      };
+    },
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise((resolve) => app.listen({ port: PORT }, resolve));
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
+};
+
+startServer();
+
+//server.listen(PORT).then(() => console.log(`🚀 Server ready at ${PORT} ✅`));
