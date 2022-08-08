16da13d0356844896e31051c828c65d41dbfd2db
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-08 16:25:20 +0900

    ✨ deletePhoto resolvers 추가

---
+++ photos/deletePhoto/deletePhoto.resolvers.js
@@ -0,0 +1,36 @@
+import { protectResolver } from '../../users/users.utils';
+
+export default {
+  Mutation: {
+    deletePhoto: protectResolver(async (_, { id }, { loggedInUser }) => {
+      const photo = await client.photo.findUnique({
+        where: {
+          id,
+        },
+        select: {
+          userId: true,
+        },
+      });
+      if (!photo) {
+        return {
+          ok: false,
+          error: 'Photo not found',
+        };
+      } else if (photo.userId !== loggedInUser.id) {
+        return {
+          ok: false,
+          error: 'You are not allowed to delete this photo',
+        };
+      } else {
+        await client.photo.delete({
+          where: {
+            id,
+          },
+        });
+        return {
+          ok: true,
+        };
+      }
+    }),
+  },
+};
