71528b3401e234647a6788475e20ac1a4d67cce6
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-08 17:03:20 +0900

    🎨 MutationResponse 적용

--- photos/deletePhoto/deletePhoto.typeDefs.js
+++ photos/deletePhoto/deletePhoto.typeDefs.js
@@ -1,11 +1,7 @@
 import { gql } from 'apollo-server';
 
 export default gql`
-  type DeletePhotoResult {
-    ok: Boolean!
-    error: String
-  }
   type Mutation {
-    deletePhoto(photoId: Int!): DeletePhotoResult!
+    deletePhoto(photoId: Int!): MutationResponse!
   }
 `;
